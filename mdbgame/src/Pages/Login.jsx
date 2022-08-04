f849f8a062212642bd47665c1d76e489a838ef94
hudsonbay97@gmail.com <hudsonbay97@gmail.com>
2022-08-04 17:45:24 +0200

    Changed title of the html and description of the app

--- mdbgame/src/Pages/Login.jsx
+++ mdbgame/src/Pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import { Link, useNavigate } from 'react-router-dom';
 import {
   signInWithGoogle,
